f65c0a56c7e4a836dda1147d433b6ac74c350b15
Zenilton Pereira <zeniltonp0@gmail.com>
2026-03-06 19:45:37 -0300

    finalizando estilo nos campos

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,19 +3,30 @@
         <h1 class="text-2xl text-center">Registro</h1>
         <div class="block text-sm font-medium text-gray-700">
             <label>Nome</label>
-            <input type="text">
+            <input type="text" 
+            required
+            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 outline-blue-500">
         </div>
         <div class="block text-sm font-medium text-gray-700">
             <label>Email</label>
-            <input type="text">
+            <input type="text"
+            required
+            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 outline-blue-500">
         </div>
         <div class="block text-sm font-medium text-gray-700">
             <label>Senha</label>
-            <input type="text">
+            <input type="text"
+            required
+            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 outline-blue-500">
         </div>
         <div class="block text-sm font-medium text-gray-700">
             <label>Tipo de Usuário</label>
-            <input type="text">
+            <select type="text"
+            required
+            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 outline-blue-500">
+                <option>Admin</option>
+                <option>Usuário</option>
+        </select>
         </div>
         <button>
             Criar conta
