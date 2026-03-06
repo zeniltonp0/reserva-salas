0258716dc55366050ed8d8ece1f70c6af15a42a4
Zenilton Pereira <zeniltonp0@gmail.com>
2026-03-06 19:39:23 -0300

    criando campos do form e aplicando estilo inicial

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,24 @@
 <template>
-    <aside>
-        register
-    </aside>
+    <form class="space-y-6">
+        <h1 class="text-2xl text-center">Registro</h1>
+        <div class="block text-sm font-medium text-gray-700">
+            <label>Nome</label>
+            <input type="text">
+        </div>
+        <div class="block text-sm font-medium text-gray-700">
+            <label>Email</label>
+            <input type="text">
+        </div>
+        <div class="block text-sm font-medium text-gray-700">
+            <label>Senha</label>
+            <input type="text">
+        </div>
+        <div class="block text-sm font-medium text-gray-700">
+            <label>Tipo de Usuário</label>
+            <input type="text">
+        </div>
+        <button>
+            Criar conta
+        </button>
+    </form>
 </template>
